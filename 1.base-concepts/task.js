8aa3969b926421d19894a086f98ecf1f192d9958
Sergey Stepanov <ser1653285@yandex.ru>
2021-07-01 08:58:46 +0300

    Решение ДЗ1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,28 @@
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+    let arr = [];
+    // код для задачи №1 писать здесь
+
+    let discriminant = b ** 2 - 4 * a * c;
+    let squareRootDiscriminant = Math.sqrt(discriminant); // Math.sqrt(number) расчет корня
+
+    if (discriminant === 0) {
+        arr.push((-b - squareRootDiscriminant) / (2 * a));
+    } else if (discriminant > 0) {
+        arr.push(
+            (-b + squareRootDiscriminant) / (2 * a),
+            (-b - squareRootDiscriminant) / (2 * a)
+        );
+    }
+
+    return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+    let totalAmount;
 
-  // код для задачи №2 писать здесь
+    // код для задачи №2 писать здесь
 
-  return totalAmount;
+    return totalAmount;
 }
