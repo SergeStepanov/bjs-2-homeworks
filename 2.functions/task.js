1b225ee271e44914aee69fb6e4adb4575121e660
Sergey Stepanov <ser1653285@yandex.ru>
2021-07-04 23:29:59 +0300

    решение ДЗ2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,60 @@
+'use strict';
+
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+    let min = 101;
+    let max = -101;
+    let sum = 0;
+    let avg = 0;
 
-  // Ваш код
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
 
-  return { min:min, max:max, avg:avg };
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+
+        sum += arr[i];
+        avg = (sum / arr.length).toFixed(2);
+    }
+
+    // Ваш код
+
+    return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+    let sum = 0;
 
-  // Ваш код
+    // Ваш код
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
 
-  return sum;
+    return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+    let max = 0;
 
-  // Ваш кода
-  // for ...
-  
-  return max
+    for (let i = 0; i < arrOfArr.length; i++) {
+        let sum = func(arrOfArr[i]);
+
+        if (sum > max) {
+            max = sum;
+        }
+    }
+
+    return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+    // Ваш код
+    let result = Math.abs(Math.max(...arr) - Math.min(...arr));
+
+    return result;
 }
