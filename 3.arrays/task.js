cf575c1d1efaf0f3521e53656419fe2c90977fcb
Sergey Stepanov <ser1653285@yandex.ru>
2021-07-05 11:55:13 +0300

    Решение Дз3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,22 @@
+'use strict';
+
 function compareArrays(arr1, arr2) {
-  let result;
+    let result;
 
-  // Ваш код
+    if (arr1.length === arr2.length) {
+        result = arr1.every((item, index) => item === arr2[index]);
+    } else {
+        result = false;
+    }
 
-  return result; // boolean
+    return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+    let resultArr = arr
+        .filter((index) => index > 0)
+        .filter((index) => index % 3 === 0)
+        .map((index) => index * 10);
 
-  // Ваш код
-
-  return resultArr; // array
+    return resultArr; // array
 }
