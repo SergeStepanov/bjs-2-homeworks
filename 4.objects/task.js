74cf83c1dc4fdf59507b0bf7899624083fc187c1
Sergey Stepanov <ser1653285@yandex.ru>
2021-07-05 21:11:46 +0300

    Решение Дз4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,51 @@
-function Student(name, gender, age) {
-    // Ваш код
+'use strict';
 
+function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+    this.subject = subjectName;
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+    if (this.marks === undefined) {
+        this.marks = [];
+        this.marks.push(mark);
+    } else {
+        this.marks.push(mark);
+    }
+};
+
+Student.prototype.addMarks = function (...markArr) {
+    if (this.marks === undefined) {
+        this.marks = [];
+        for (let ind of markArr) {
+            this.marks.push(ind);
+        }
+    } else {
+        markArr.forEach(function (val) {
+            this.marks.push(val);
+        });
+    }
+};
+
+Student.prototype.getAverage = function () {
+    let sum = 0;
+    let avg = 0;
+
+    this.marks.forEach(function (value) {
+        sum += value;
+    });
+    avg = sum / this.marks.length;
+
+    return avg;
+};
+
+Student.prototype.exclude = function (reason) {
+    delete this.marks;
+    delete this.subject;
+    this.excluded = reason;
+};
