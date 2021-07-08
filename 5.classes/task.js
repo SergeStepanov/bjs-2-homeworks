3079cc798a805dacfea98a850b43b4a5cc43700e
Sergey Stepanov <ser1653285@yandex.ru>
2021-07-08 20:47:49 +0300

    для ноута пулл

---
+++ 5.classes/task.js
@@ -0,0 +1,116 @@
+'use strict';
+
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+
+    fix() {
+        this.state *= 1.5;
+    }
+
+    set state(num) {
+        if (num < 0) {
+            this.state = 0;
+        } else if (num > 100) {
+            this.state = 100;
+        } else {
+            this._state = num;
+        }
+    }
+
+    get state() {
+        return this._state;
+    }
+}
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = 'magazine';
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = 'book';
+    }
+}
+
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = 'novel';
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = 'fantastic';
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = 'detective';
+    }
+}
+
+// Библиотека
+
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (this.state < 30) {
+            return this.books;
+        } else {
+            return this.books.push(book);
+        }
+    }
+
+    // findBookBy(type, value) {
+    //     this.books.forEach(function (item) {
+    //         for (let key in item) {
+    //             if (key === type && this.item[key] === value) {
+    //                 return this.item;
+    //             }
+    //         }
+    //     });
+    //     return null;
+    // } //не работает. Почему?
+
+    findBookBy(type, value) {
+        for (let i = 0; i < this.books.length; i++) {
+            for (let key in this.books[i]) {
+                if (key === type && this.books[i][key] === value) {
+                    return this.books[i];
+                }
+            }
+        }
+        return null;
+    }
+
+    giveBookByName(bookName) {
+        for (let i = 0; i < this.books.length; i++) {
+            if (this.books[i].name === bookName) {
+                let deleteBook = this.books.indexOf(this.books[i]);
+                this.books.splice(deleteBook, 1);
+                return;
+                // return this.books[i];
+            }
+        }
+        return null;
+    }
+}
