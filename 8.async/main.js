b09a9ed1521150a6b79af3469ac3790082d928a4
Sergey Stepanov <ser1653285@yandex.ru>
2021-07-23 16:52:38 +0300

    add main

--- 8.async/main.js
+++ 8.async/main.js
@@ -1,1 +1,24 @@
 // тут вы можете вызывать функции из task.js
+
+const alarmTest = new AlarmClock();
+
+alarmTest.addClock('09:00', () => console.log('Подъём!!!!!!'), 1);
+alarmTest.addClock('09:02', () => console.log('Вставай!!!!!!'), 1);
+alarmTest.addClock('09:02', () => console.log('Вставай давай!!!!!!'), 2);
+alarmTest.addClock('09:03', () => console.log('Проспал!!!!!!'), 3);
+
+alarmTest.printAlarms();
+
+console.log(alarmTest);
+
+console.log(`=======---======`);
+
+alarmTest.removeClock(2);
+
+console.log(alarmTest);
+
+console.log(`=======---======`);
+
+alarmTest.clearAlarms();
+
+console.log(alarmTest);
